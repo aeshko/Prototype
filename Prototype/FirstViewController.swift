5d030a5ecde0c2d45c38aea6f3096c4e753e47bd
Алексей Ещенко <alexeyeschenko@gmail.com>
2018-11-02 20:09:06 +0500

    create prototype

---
+++ Prototype/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Prototype
+//
+//  Created by Алексей Ещенко on 02.11.2018.
+//  Copyright © 2018 Алексей Ещенко. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Prototype/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Prototype
+//
+//  Created by Алексей Ещенко on 02.11.2018.
+//  Copyright © 2018 Алексей Ещенко. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
